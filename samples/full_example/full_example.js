4c1121ec5af4ec260b753db6f101690679483ab3
Gian Marco Cabiato <gmcabiato@gmail.com>
2024-05-09 16:28:35 +0100

    feat: documentation update (#13)

--- samples/full_example/full_example.js
+++ samples/full_example/full_example.js
@@ -1,63 +1,171 @@
-const getPaymentData = async() => {
-        
-    // Auth to get token
-    // ATTENTION: MAKE SURE NOT TO INCLUDE THIS AUTHENTICATION SNIPPET IN YOUR CLIENTSIDE APPLICATION
-    // THIS HAS BEEN DONE FOR DEMONSTRATION PURPOSES ONLY!!!!
-    const tokenRes = await fetch('https://<ENVIRONMENT_AUTH_URL>/auth/realms/toonie/protocol/openid-connect/token', {
-        method: 'POST',
+const baseUrl = "https://<ENVIRONMENT_API_URL>";
+const successUrl = "<SUCCESS_PAGE_URL>";
+const errorUrl = "<ERROR_PAGE_URL>";
+
+// Auth to get token
+// ATTENTION: MAKE SURE NOT TO INCLUDE THIS AUTHENTICATION SNIPPET IN YOUR CLIENTSIDE APPLICATION
+// THIS HAS BEEN DONE FOR DEMONSTRATION PURPOSES ONLY!!!!
+const getTokenData = async () => {
+    const tokenRes = await fetch(`${baseUrl}/auth/realms/toonie/protocol/openid-connect/token`, {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
+            "Content-Type": "application/x-www-form-urlencoded",
         },
         body: new URLSearchParams({
             "grant_type": "password",
-            "client_id": "pay-with-toonie",
-            "username": "customerusername",
-            "password": "customerpassword",
-        })
+            "client_id": "paywithtoonie-client",
+            "username": "<USERNAME>",
+            "password": "<PASSWORD>",
+        }),
     });
 
-    const tokenData = await tokenRes.json()
+    return await tokenRes.json();
+}
 
-        //Create payment intent
-    const res  = await fetch('https://<ENVIRONMENT_API_URL>/offers/v1/payments', {
-        method: 'POST',
+// CREATE PAYMENT SESSION
+const createPaymentSession = async (amount, currency, reason) => {
+    const tokenData = await getTokenData();
+
+    const res = await fetch(`${baseUrl}/acquiring/v1/payment`, {
+        method: "POST",
         headers: {
             Authorization: ` Bearer ${tokenData.access_token}`,
             "content-type": "application/json",
         },
         body: JSON.stringify({
-            "amount": "0.05",
-            "reason": "Test Payment 01",
-            "destinationWalletId": "<MERCHANTWALLETID>",
-            "transactionCurrency": "EUR"
+            "amount": amount,
+            "currency": currency,
+            "reason": reason,
+            "successUrl": successUrl,
+            "errorUrl": errorUrl,
         })
     })
 
     const data = await res.json()
-    
-    // Data to be consumed by the SDK
-    return { 
-        paymentSessionId: data.paymentSessionId,
-        otp: data.otp,
-        paymentShortReference: data.shortReference
-        }
-    };
 
-const failurePaymentCallback = (err) => {
-    console.log('userError', err)
+    return {
+        paymentSessionId: data.sessionId,
+        amount: data.amount,
+        currency: data.currency,
+        successUrl: data.successUrl,
+        errorUrl: data.errorUrl,
+        reason: data.reason,
+        otp: data.otp,
+        paymentShortReference: data.shortReference,
+        displayName: data.displayName,
+    }
+};
+
+// GET PAYMENT DATA BY SESSION ID
+const fetchPaymentDataBySessionId = async (paymentSessionId) => {
+    const res = await fetch(`${baseUrl}/acquiring/v1/payment/paymentSession/${paymentSessionId}`, {
+        method: "GET",
+        headers: {
+            "content-type": "application/json",
+        },
+    })
+
+    const data = await res.json()
+
+    return {
+        paymentSessionId: data.sessionId,
+        status: data.status,
+        amount: data.amount,
+        currency: data.currency,
+        reason: data.reason,
+        successUrl: data.successUrl,
+        errorUrl: data.errorUrl,
+        displayName: data.displayName,
+    }
 }
+
+// INITIATE A PAYMENT
+const initiatePayment = async (paymentSessionId, amount, currency, provider) => {
+    const res = await fetch(`${baseUrl}/acquiring/v1/payment/initiate`, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+        },
+        body: JSON.stringify({
+            "paymentSessionId": paymentSessionId,
+            "provider": provider,
+        }),
+    });
+
+    const data = await res.json();
+
+    return {
+        "amount": data.amount,
+        "currency": data.currency,
+        "reason": data.reason,
+        "clientSecret": data.clientSecret,
+        "stripePaymentIntentId": data.paymentIntentId,
+        "feeAmount": data.feeAmount,
+        "otp": data.provider.otp,
+        "offersSessionId": data.provider.paymentOfferSessionId,
+        "paymentShortReference": data.provider.shortReference,
+    };
+}
+
+// UPDATE PAYMENT
+const updatePayment = async (paymentSessionId, paymentStatus) => {
+    return await fetch(`${baseUrl}/acquiring/v1/payment/${paymentSessionId}/status/${paymentStatus}`, {
+        method: "PUT",
+        headers: {
+            "content-type": "application/json",
+        }
+    });
+}
+
+/**
+ * CARD
+ */
+
+  // APPROVE CARD PAYMENT
+const approveCardPayment = async (paymentSessionId) => {
+    return await fetch(`${baseUrl}/acquiring/v1/payment/${paymentSessionId}/approve`, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+        },
+    });
+}
+
+/**
+ * CALLBACKS
+ */
 
 const successPaymentCallback = (data) => {
-    console.log('Success!!', data)
+    console.log("success", data)
 }
 
-const baseUrl = "https://<ENVIRONMENT_API_URL>";
+const failurePaymentCallback = (err) => {
+    console.log("userError", err);
+}
+
+const onModalClose = (error) => {
+    if (error) {
+        console.error("paymentError", error)
+    }
+}
+
+const renderPayWithToonieButton = true;
+const renderPayWithCardButton = true;
 
 const options = {
-    getPaymentData,
+    getTokenData,
+    createPaymentSession,
+    fetchPaymentDataBySessionId,
+    initiatePayment,
+    updatePayment,
+    approveCardPayment,
     successPaymentCallback,
     failurePaymentCallback,
-    baseUrl                 // Optional Parameter for Testing environments
+    onModalClose,
+    baseUrl,
+    renderPayWithToonieButton,
+    renderPayWithCardButton,
 }
+
 // builds the UI for the form
 PayWithToonie.render(document.querySelector("#toonie-button"), options);
